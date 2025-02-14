fcfab756e16a1d1d061e6ee36b40b3c3aa508d87
Joshua Book <joshua.book@gmail.com>
2025-02-13 16:11:09 -0800

    Clean up handle sign out func.

--- app/account.tsx
+++ app/account.tsx
@@ -19,7 +19,18 @@ export default function Account() {
     loading,
     updateProfile,
   } = useProfile();
-  const { session } = useSessionStore();
+  const { session, clearSession } = useSessionStore();
+
+  async function handleSignOut() {
+    const { error } = await supabase.auth.signOut()
+    if (error) {
+      Alert.alert('Error logging out:', error.message)
+      return;
+    } else {
+      clearSession()
+      router.replace('/auth')
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -52,7 +63,7 @@ export default function Account() {
       </View>
 
       <View style={styles.verticallySpaced}>
-        <Button title="Sign Out" onPress={() => {supabase.auth.signOut(); router.replace('auth')}} />
+        <Button title="Sign Out" onPress={handleSignOut} />
       </View>
     </View>
   );
